b3f28a662db5a0b79f38915a07b8977f30fc878f
freddy-mendez <freddy.mendez@misena.edu.co>
2021-11-16 21:55:12 -0500

    Creación de interfaces del proceso de Libros

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { CrearComponent } from './components/libros/crear/crear.component';
+import { ListarComponent } from './components/libros/listar/listar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'listar-libros', component: ListarComponent},
+  {path: 'crear-libro', component: CrearComponent},
+  {path: '**', redirectTo: '', pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
